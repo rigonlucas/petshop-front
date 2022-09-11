2d322dc828a003c3ba7bde2632ed6395a23c1a90
Lauro Rigon <lauro.rigon@odig.net>
2022-09-10 22:21:50 -0300

    melhorias visuais e de data inicial do calendario

--- src/pages/schedule/ScheduleIndex.vue
+++ src/pages/schedule/ScheduleIndex.vue
@@ -57,7 +57,7 @@
                         </q-btn>
                     </div>
                     <div class="col-12 col-sm-4 text-center">
-                        <p class="text-h5">{{ currentDateString }}</p>
+                        <p class="text-h5 text-grey-9">{{ currentDateString }}</p>
                     </div>
                     <div class="col-12 flex justify-end col-sm-4">
                         <select-callendar-view
@@ -93,7 +93,7 @@ import { CalendarOptions } from '@fullcalendar/core'
 import listPlugin from '@fullcalendar/list'
 import localePtBr from '@fullcalendar/core/locales/pt-br'
 import ScheduleService from 'src/features/schedule/services/ScheduleService'
-import { differenceInMinutes, format, getDay, isPast } from 'date-fns'
+import { differenceInMinutes, format, getDay, isPast, parseISO } from 'date-fns'
 import { ScheduleFormData } from 'src/features/schedule/models/ScheduleForm'
 import { useRouter } from 'vue-router'
 import { reactive, ref } from 'vue'
@@ -168,7 +168,7 @@ const calendarOptions: CalendarOptions = {
     initialView: scheduleStore.selectedView,
     height: '100%',
     headerToolbar: false,
-    initialDate: filters.start_at || undefined,
+    initialDate: (filters.start_at && parseISO(filters.start_at)) || undefined,
     plugins: [
         listPlugin,
         timeGridPlugin,
@@ -200,7 +200,21 @@ const calendarOptions: CalendarOptions = {
     },
 }
 </script>
+<style lang="scss">
+.fc {
+    .fc-timegrid-slot {
+        padding: 10px 0;
+        border-width: 1px;
+        border-color: $grey-4;
+        border-bottom-style: solid;
+    }
 
+    .fc-col-header-cell-cushion {
+        font-size: 1.3rem;
+        color: $grey-9;
+    }
+}
+</style>
 <style scoped lang="scss">
 .schedule-popover {
     height: 100%;
